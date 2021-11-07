1255e5cb779d7894a847074660ce1a91907dcd18
Shiam Baghla <shivambaghla93@gmail.com>
2021-11-07 20:18:39 +0530

    tasks module designed

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,7 @@
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private taskService: TasksService){}
+}
