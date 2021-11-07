175bb497282158d58bd19a1396dfca9e93f7f029
Shiam Baghla <shivambaghla93@gmail.com>
2021-11-07 20:23:02 +0530

    feat: ability to get all tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -4,4 +4,9 @@ import { TasksService } from './tasks.service';
 @Controller('tasks')
 export class TasksController {
     constructor(private taskService: TasksService){}
+
+    @Get()
+    getAllTasks() {
+        return this.taskService.getAllTasks();
+    }
 }
